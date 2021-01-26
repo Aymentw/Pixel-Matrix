125b67a354052a02b0993cb932aeb475313850cf
Aymentw <ben.chaabane.aymen@gmail.com>
2021-01-26 19:52:04 +0100

    Commiting

---
+++ src/Matrix.js
@@ -0,0 +1,93 @@
+import PixelGrid from "react-pixel-grid";
+import React, { Component } from "react";
+import "./Matrix.css"
+
+class Matrix extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            showImage: false,
+            nmbre: 0,
+            matrix: [],
+        };
+    }
+
+    handleChange(event) {
+        this.setState({
+            nmbre: event.target.value,
+        });
+    }
+
+    generateImage() {
+        console.log('Generate');
+        let myarr = [];
+        let createarray1 = function (rowCount, colCount) {
+            for (let i = 0; i < rowCount; i++) {
+                let row = []
+                for (let j = 0; j < colCount; j++) {
+                    let col = Math.floor(Math.random() * 2);
+                    row.push(col);
+                }
+                myarr.push(row);
+            }
+            return myarr;
+        }
+        createarray1(this.state.nmbre * 2, this.state.nmbre * 2);
+        this.setState({
+            matrix: myarr,
+            showImage: true,
+        });
+    }
+
+    reverseImage() {
+        console.log('Reverse')
+        this.setState({
+            matrix: this.state.matrix.reverse(),
+        });
+    }
+
+    flipImage() {
+        console.log('Flip')
+        this.setState({
+            matrix: this.state.matrix.map(row => row.reverse()),
+        });
+    }
+
+    render() {
+
+        return (
+            <>
+                <div className="container-image">
+                    {this.state.showImage
+                        ? <PixelGrid
+                            data={this.state.matrix}
+                            options={{
+                                size: 30,
+                                padding: 2,
+                                background: [0, 0, 0],
+                            }}
+                        />
+                        : null
+                    }
+                </div>
+                <div className="container-buttons">
+                    <input type="number" name="nmbre" value={this.state.nmbre}
+                        onChange={this.handleChange.bind(this)} />
+                    <button onClick={() => this.generateImage()}>
+                        Generate
+                </button>
+                    <button onClick={() => this.flipImage()}>
+                        Flip(Horizontal)
+                </button>
+                    <button onClick={() => this.reverseImage()}>
+                        Invert
+                </button>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Matrix;
+
